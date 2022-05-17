a045587beea927e63ec6b353e6ba7a3fbb3a6394
Oscar Castañeda <ext_ojcastanedah@falabella.cl>
2022-05-17 13:35:02 -0500

    learning list css

--- app.js
+++ app.js
@@ -4,32 +4,33 @@ TITLE
 </dt>
 <dd>
 VERS
+<button class="btn">Clic</button>
 </dd>`;
-let content = "";
+let content = '';
 for (let i = 0; i < texts.length; i++) {
   let entry = textsTemplate
     .replace(/TITLE/g, texts[i].title)
     .replace(/VERS/g, texts[i].vers);
   content += entry;
 }
-document.getElementById("content").innerHTML = content;
+document.getElementById('content').innerHTML = content;
 //Month
 const contentMonth = month;
-document.getElementById("month").innerHTML = contentMonth;
+document.getElementById('month').innerHTML = contentMonth;
 //Questions
 const qTemplates = `<li>
 QUESTION
 </li>`;
-let questionsContent = "";
+let questionsContent = '';
 for (let i = 0; i < questions.length; i++) {
   let entry = qTemplates.replace(/QUESTION/g, questions[i]);
   questionsContent += entry;
 }
-document.getElementById("questions").innerHTML = questionsContent;
+document.getElementById('questions').innerHTML = questionsContent;
 
 // Registering Service Worker
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/PWA-elencuentro/sw.js");
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/PWA-elencuentro/sw.js');
 }
 
 let deferredPrompt;
